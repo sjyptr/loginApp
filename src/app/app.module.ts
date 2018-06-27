ec0dff84e6b9866638a73806afac0b2e58f7dcab
Sanjay Patidar <patidarsanjay88@gmail.com>
2018-06-27 16:58:08 +0530

    add 3 new component and added 1 service, created login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router'
 
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { AuthService} from './auth.service'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+
+{
+  path: 'login',
+  component: LoginComponent
+},
+{
+  path: 'admin',
+  component: AdminComponent
+},
+{
+  path: '',
+  component: HomeComponent
+}
+
+    ])
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
